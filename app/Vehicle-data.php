8693da472dbbe40291ee59f7fe689cfbb9f03ea7
minhanhbg <vu.anh147852@gmail.com>
2022-05-06 22:50:21 +0700

    version 1

--- app/Vehicle-data.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $severName = "DESKTOP-LH0LVCT\ZEROTWO";
-    $connectionInfo = array("Database"=>"data-garbage","UID"=>"sa","PWD"=>"zerotwo002");
-    $conn = sqlsrv_connect($severName,$connectionInfo);
-    if($conn){
-        
-    }
-?>
